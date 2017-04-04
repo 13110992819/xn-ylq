5397b9e894b16b06eb401144f7412108973c6326
pyez1158 <pyez1158@163.com>
2017-04-04 22:10:13 +0800

    脚本整理

--- docs/schema/schema_xn_zhpay.sql
+++ docs/schema/schema_xn_zhpay.sql
@@ -214,6 +214,7 @@ CREATE TABLE `tsys_dict` (
   `updater` varchar(32) NOT NULL COMMENT '更新人',
   `update_datetime` datetime NOT NULL COMMENT '更新时间',
   `remark` varchar(255) DEFAULT NULL COMMENT '备注',
+  `company_code` varchar(32) DEFAULT NULL COMMENT '公司编号',
   `system_code` varchar(32) DEFAULT NULL COMMENT '系统编号',
   PRIMARY KEY (`id`) COMMENT '数据字典'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
