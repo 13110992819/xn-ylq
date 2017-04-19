70fe0e0481e0ea2cb8bed84f1a49edc6eed8d44a
pyez1158 <pyez1158@163.com>
2017-04-19 22:24:28 +0800

    bug 修复

--- docs/truncate/delete_xn_zhpay.sql
+++ docs/truncate/delete_xn_zhpay.sql
@@ -1,15 +1,15 @@
 SET SQL_SAFE_UPDATES = 0;
-delete from tsys_config where system_code = 'CD-CZH000001';
-delete from tsys_dict where system_code = 'CD-CZH000001';
+delete from tsys_config where system_code = 'CD-CCG000007';
+delete from tsys_dict where system_code = 'CD-CCG000007';
 
-delete from tyqs_hzb where system_code = 'CD-CZH000001';
-delete from tyqs_hzb_mgift where system_code = 'CD-CZH000001';
+delete from tyqs_hzb where system_code = 'CD-CCG000007';
+delete from tyqs_hzb_mgift where system_code = 'CD-CCG000007';
 
-delete from tyqs_hzb_template where system_code = 'CD-CZH000001';
-delete from tyqs_hzb_yy where system_code = 'CD-CZH000001';
+delete from tyqs_hzb_template where system_code = 'CD-CCG000007';
+delete from tyqs_hzb_yy where system_code = 'CD-CCG000007';
 
-delete from tyydb_jewel where system_code = 'CD-CZH000001';
-delete from tyydb_jewel_record where system_code = 'CD-CZH000001';
+delete from tyydb_jewel where system_code = 'CD-CCG000007';
+delete from tyydb_jewel_record where system_code = 'CD-CCG000007';
 
-delete from tyydb_jewel_record_number where system_code = 'CD-CZH000001';
-delete from tyydb_jewel_template where system_code = 'CD-CZH000001';
+delete from tyydb_jewel_record_number where system_code = 'CD-CCG000007';
+delete from tyydb_jewel_template where system_code = 'CD-CCG000007';
