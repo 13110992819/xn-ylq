fc3af4145788f5e7ac641d11f5805794aa3d7a2b
pyez1158 <pyez1158@163.com>
2017-04-20 21:04:57 +0800

    bug 修复

--- docs/init_data/init_xn_zhpay_caigo.sql
+++ docs/init_data/init_xn_zhpay_caigo.sql
@@ -4,7 +4,7 @@ LIMIT 0, 10000
 
 -- Date: 2017-03-31 15:34
 */
-INSERT INTO `tyqs_hzb_template` (`code`,`name`,`pic`,`price`,`currency`,`period_rock_num`,`total_rock_num`,`back_amount1`,`back_amount2`,`back_amount3`,`status`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('HT20170000000000002','摇钱树','tree_big.png',100000,'CNY',1000000,100000,100000,100000,100000,'0','admin',now(),NULL,'CD-CCG000007','CD-CCG000007');
+INSERT INTO `tyqs_hzb_template` (`code`,`name`,`pic`,`price`,`currency`,`period_rock_num`,`total_rock_num`,`back_amount1`,`back_amount2`,`back_amount3`,`status`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('HT20170000000000002','摇钱树','tree_big.png',100000,'CNY',1000000,100000,100000,100000,100000,'1','admin',now(),NULL,'CD-CCG000007','CD-CCG000007');
 
 /*
 -- Query: SELECT `type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code` FROM xn_zhpay.tsys_config where system_code = 'CD-CCG000007'
