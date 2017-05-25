3cf7ea4aaf84438a8d7d81ecd13777f7290f5a27
pyez1158 <pyez1158@163.com>
2017-05-25 13:35:35 +0800

    代码提交

--- src/main/java/com/cdkj/zhpay/APIServlet.java
+++ src/main/java/com/cdkj/zhpay/APIServlet.java
@@ -41,10 +41,12 @@ public class APIServlet extends HttpServlet {
             HttpServletResponse response) throws ServletException, IOException {
         logger.info("Successful Deployment");
         PrintWriter writer = response.getWriter();
-        writer.append("Version:3.1.0 \n");
-        writer.append("Description:3rd \n");
+        writer.append("Version:3.1.1 \n");
+        writer.append("Description:2nd \n");
 
         writer.append("history ******************** \n");
+        writer.append("Version:3.1.0 \n");
+        writer.append("Description:3rd \n");
         writer.append("Version:3.0.4 \n");
         writer.append("Description:1st\n");
         writer.flush();
